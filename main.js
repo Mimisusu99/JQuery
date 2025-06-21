78a69f2c5f930da65e52a3e5f8a70a1b40314f71
Anshori Suhendro <anshorishndr@gmail.com>
2025-06-21 12:43:52 +0700

    DOM Selection JQuery

---
+++ main.js
@@ -0,0 +1,10 @@
+// const el = $("p")
+// const el1 = document.getElementsByTagName("p")
+// const el2 = document.querySelectorAll("p")
+
+// const el = $("#hello")
+const el = $(".world")
+
+console.log(el)
+// console.log(el1)
+// console.log(el2)
