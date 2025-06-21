e59aa486a3a162881e289cde66f4f06ad6a204b7
Anshori Suhendro <anshorishndr@gmail.com>
2025-06-21 13:51:45 +0700

    DOM manipulation

--- main.js
+++ main.js
@@ -3,8 +3,27 @@
 // const el2 = document.querySelectorAll("p")
 
 // const el = $("#hello")
-const el = $(".world")
+// const el = $(".world")
 
-console.log(el)
+// console.log(el)
 // console.log(el1)
 // console.log(el2)
+
+// .html
+const el = $("#text")
+el.html("Hello World")
+
+// .attr()
+const inp = $("input")
+inp.attr("type", "checkbox")
+
+// add & remove class
+el.addClass("red")
+el.addClass("blue")
+el.removeClass("blue")
+
+//css()
+inp.css({
+    border: "1px solid red",
+    marginleft: "100px"
+})
